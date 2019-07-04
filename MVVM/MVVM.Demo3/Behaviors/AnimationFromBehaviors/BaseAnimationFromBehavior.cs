1a913c69a3448769322fe3b311c755afda98f37f
Никита Кузнецов <kuznetcov701@gmail.com>
2019-07-04 23:37:21 +0700

    анимация

---
+++ MVVM/MVVM.Demo3/Behaviors/AnimationFromBehaviors/BaseAnimationFromBehavior.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace MVVM.Demo3
+{
+    public class BaseAnimationFromBehavior<T> : Behavior<T> where T : UIElement
+    {
+        public AnimationTimeline Animation { get; set; }
+        public DependencyProperty Property { get; set; }
+
+        protected virtual async Task BeginAnimation()
+        {
+            AssociatedObject.BeginAnimation(Property, Animation);
+            await Task.Delay(Animation.Duration.TimeSpan);
+        }
+    }
+}
